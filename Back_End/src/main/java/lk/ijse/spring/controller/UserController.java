e7c5adec04e6e04cb7c27a4c62a98966f9b5954b
Milan-sandeepa <glmsandeepa@gmail.com>
2023-02-19 16:16:27 +0530

    admin car add form implemented

--- Back_End/src/main/java/lk/ijse/spring/controller/UserController.java
+++ Back_End/src/main/java/lk/ijse/spring/controller/UserController.java
@@ -4,10 +4,16 @@ import lk.ijse.spring.dto.UserDTO;
 import lk.ijse.spring.service.impl.UserServiceImpl;
 import lk.ijse.spring.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.view.RedirectView;
+
+import java.io.IOException;
+import java.net.URI;
 
 @RestController
 @CrossOrigin
@@ -17,11 +23,21 @@ public class UserController {
     @Autowired
     UserServiceImpl userService;
 
+    //    @PostMapping
+//    public ResponseUtil saveUser(UserDTO userDTO) {
+//
+//        userService.saveUser(userDTO);
+//
+//        return new ResponseUtil("Ok", "Successfully Saved", null);
+//    }
     @PostMapping
-    public ResponseUtil saveUser(UserDTO userDTO) {
+    public RedirectView saveUser(UserDTO userDTO){
 
         userService.saveUser(userDTO);
+        RedirectView redirectView = new RedirectView();
+        redirectView.setUrl("http://localhost:63342/Car_Rental_System/Front_End/index.html");
 
-        return new ResponseUtil("Ok", "Successfully Saved", null);
+        return redirectView;
     }
+
 }
