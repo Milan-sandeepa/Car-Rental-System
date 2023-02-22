63c846fc77538127a27945b55233fbb58b8263f6
Milan-sandeepa <glmsandeepa@gmail.com>
2023-02-22 17:31:27 +0530

    Admin ui updated

---
+++ Back_End/src/main/java/lk/ijse/spring/dto/DriverDTO.java
@@ -0,0 +1,15 @@
+package lk.ijse.spring.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Data
+public class DriverDTO {
+    private String driverID;
+
+}
